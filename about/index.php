4fdcb345ee7907a6d1986628b4dfe4ae1023d9b1
Fahmi <fahmiaji123.fak@gmail.com>
2023-06-21 00:36:08 +0700

    sample

---
+++ about/index.php
@@ -0,0 +1,15 @@
+<?php include('../template/header.php');?>
+<section id="main-content">
+    <section class="wrapper site-min-height">
+        <div class="row content-panel">
+            <h2 class="centered"><i class="fa fa-info-circle"></i> Tentang E-KATALOG</h2><hr>
+            <div class="col-md-8">
+                <p>Selamat datang di E-KATALOG, perpustakaan online yang menyediakan akses mudah ke berbagai buku, artikel, dan sumber daya lainnya. Tujuan kami adalah memfasilitasi penemuan pengetahuan dan meningkatkan minat membaca.</p>
+                <p>E-KATALOG hadir untuk memenuhi kebutuhan pembelajaran Anda. Baik Anda seorang pelajar, peneliti, atau sekadar ingin memperluas wawasan, kami menyediakan koleksi yang luas di rak virtual kami. Jelajahi berbagai kategori, temukan penulis terkenal, dan nikmati petualangan literasi yang seru.</p>
+                <p>Kami memiliki tim yang berdedikasi untuk mengelola dan memperbarui koleksi kami secara berkala. Kami berkomitmen untuk menyediakan koleksi yang beragam, inklusif, dan selalu diperbarui, sesuai dengan berbagai minat dan topik yang diminati.</p>
+                <p>Bergabunglah dengan E-KATALOG sekarang dan bukalah pintu pengetahuan yang tak terbatas. Mari bersama-sama mengeksplorasi dunia pengetahuan, membaca, dan belajar di E-KATALOG!</p>
+            </div>
+        </div>
+    </section>
+</section>
+<?php include('../template/footer.php');?>
---
+++ modul_buku/proses-list-buku.php
@@ -0,0 +1,22 @@
+<?php
+if (session_status()!=PHP_SESSION_ACTIVE) {
+    session_start();
+}
+if (!isset($_SESSION['user'])){
+    header('Location: ../login.php');
+    exit();
+}
+
+include '../config.php';
+
+$query = "SELECT buku.*,kategori.kategori_nama
+    FROM buku
+    JOIN kategori
+    ON buku.kategori_id = kategori.kategori_id
+    ORDER BY buku.buku_kode ASC";
+$hasil = mysqli_query($db, $query);
+$data_buku = array();
+
+while ($row = mysqli_fetch_assoc($hasil)) {
+    $data_buku[] =  $row;
+}
